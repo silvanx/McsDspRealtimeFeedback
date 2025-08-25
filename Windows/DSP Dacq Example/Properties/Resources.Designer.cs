49af84d3bc209c8b589d28887ca69b713841a6b8
Kuba Orłowski <kuba.orlowski@protonmail.com>
2025-08-25 19:51:10 +0100

    Rename projects to make more sense; add program icon

--- Windows/DSP Dacq Example/Properties/Resources.Designer.cs
+++ Windows/DSP Dacq Example/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MCS_USB_Windows_Forms_Application1.Properties {
+namespace Closed_loop_feedback_gui.Properties {
     using System;
     
     
@@ -19,7 +19,7 @@ namespace MCS_USB_Windows_Forms_Application1.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -39,7 +39,7 @@ namespace MCS_USB_Windows_Forms_Application1.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MCS_USB_Windows_Forms_Application1.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Closed_loop_feedback_gui.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
